d9445bf131a0b1ee06566eede650f329b50be8ee
elizabethpurtell <epurtell.CF23@gmail.com>
2023-06-22 10:53:36 -0700

    Updated styling, added more information to education

--- js/app.js
+++ js/app.js
@@ -2,57 +2,57 @@
 
 // Ask user their name
 
-let userName = prompt('What is your name?');
+// let userName = prompt('What is your name?');
 
 // Display that name back to the user through a custom greeting welcoming them to your site.
 
-alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
+// alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
 
 // Ask user 5 yes/no questions
 
-let questionOneGuess = prompt('Am I an Artist?').toLowerCase();
+// let questionOneGuess = prompt('Am I an Artist?').toLowerCase();
 
-if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
-  alert('You are correct!');
-} else if(questionOneGuess === 'n' || questionOneGuess === 'no'){
-  alert('Sorry you are wrong, I have been an Artist for over 20 years');
-}
+// if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
+//   alert('You are correct!');
+// } else if(questionOneGuess === 'n' || questionOneGuess === 'no'){
+//   alert('Sorry you are wrong, I have been an Artist for over 20 years');
+// }
 
-let questionTwoGuess = prompt('Do I spend a lot of time learning new things?').toLowerCase();
+// let questionTwoGuess = prompt('Do I spend a lot of time learning new things?').toLowerCase();
 
-if(questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
-  alert('You are correct!');
-} else if(questionTwoGuess === 'n' || questionTwoGuess === 'no'){
-  alert('Sorry you are wrong, I consider myself a Lifetime Learner and love to explore!');
-}
+// if(questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
+//   alert('You are correct!');
+// } else if(questionTwoGuess === 'n' || questionTwoGuess === 'no'){
+//   alert('Sorry you are wrong, I consider myself a Lifetime Learner and love to explore!');
+// }
 
-let questionThreeGuess = prompt('Have I ever worked as a nurse?').toLowerCase();
+// let questionThreeGuess = prompt('Have I ever worked as a nurse?').toLowerCase();
 
-if(questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
-  alert('You are correct!');
-} else if(questionThreeGuess === 'n' || questionThreeGuess === 'no'){
-  alert('Sorry you are wrong, I worked for many years in healthcare to pay my way through college.');
-}
+// if(questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
+//   alert('You are correct!');
+// } else if(questionThreeGuess === 'n' || questionThreeGuess === 'no'){
+//   alert('Sorry you are wrong, I worked for many years in healthcare to pay my way through college.');
+// }
 
-let questionFourGuess = prompt('Did I complete a bachelors degree in my 40s?').toLowerCase();
+// let questionFourGuess = prompt('Did I complete a bachelors degree in my 40s?').toLowerCase();
 
-if(questionFourGuess === 'y' || questionFourGuess === 'yes'){
-  alert('You are correct!');
-} else if(questionFourGuess === 'n' || questionFourGuess === 'no'){
-  alert('Sorry you are wrong, I went back to University after age 40 and completed my Bachelors in Design at age 44!');
-}
+// if(questionFourGuess === 'y' || questionFourGuess === 'yes'){
+//   alert('You are correct!');
+// } else if(questionFourGuess === 'n' || questionFourGuess === 'no'){
+//   alert('Sorry you are wrong, I went back to University after age 40 and completed my Bachelors in Design at age 44!');
+// }
 
-let questionFiveGuess = prompt('Will I complete another degree before 50?').toLowerCase();
+// let questionFiveGuess = prompt('Will I complete another degree before 50?').toLowerCase();
 
-if(questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
-  alert('You are correct!');
-} else if(questionFiveGuess === 'n' || questionFiveGuess === 'no'){
-  alert('Sorry you are wrong, I plan to complete a Masters degree by age 50!');
-}
+// if(questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
+//   alert('You are correct!');
+// } else if(questionFiveGuess === 'n' || questionFiveGuess === 'no'){
+//   alert('Sorry you are wrong, I plan to complete a Masters degree by age 50!');
+// }
 
 // Display users name back to them in a final message
 
-alert(`Thank you for answering my questions ${userName}! Please have a look-see through my About Me page to learn more about me!`);
+// alert(`Thank you for answering my questions ${userName}! Please have a look-see through my About Me page to learn more about me!`);
 
 
 
